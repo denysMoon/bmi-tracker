3b143311632e384c4fc964a9c72858eb520d126d
Denys <ulvanskyidenys@gmail.com>
2021-10-24 07:50:05 +0300

    code boilerplate

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Normalize } from "styled-normalize";
+import GlobalStyle from "./style/globalStyles";
+
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./redux/reducers/rootReducer";
+import thunk from "redux-thunk";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <Normalize />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/style/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata&display=swap');
+
+  body{
+    font-family: 'Inconsolata', monospace;
+  }
+`;
+
+export default GlobalStyle;

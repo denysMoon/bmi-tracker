ec5358b2c45ba22c0c14e8353cc67a6be2186181
Denys <ulvanskyidenys@gmail.com>
2021-10-25 21:36:05 +0300

    add color patern

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,6 +1,15 @@
 export const theme = {
   color: {
-    main: "#464646",
-    secondary: "yellow",
+    primary: {
+      main: "#f3e5f5",
+      light: "#Light",
+      dark: "#c0b3c2",
+    },
+    secondary: {
+      main: "#d0c0e8",
+      light: "#fff3ff",
+      dark: "#9f90b6",
+    },
+    text: "#3e2723",
   },
 };
